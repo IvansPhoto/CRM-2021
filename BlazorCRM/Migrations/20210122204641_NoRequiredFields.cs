7790bd5d789f82fa5afba5023ee6f46f8a1ef4ca
Ivan <gutehaltung@gmail.com>
2021-01-22 23:56:55 +0300

    Changed models.

---
+++ BlazorCRM/Migrations/20210122204641_NoRequiredFields.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorCRM.Migrations
+{
+    public partial class NoRequiredFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
